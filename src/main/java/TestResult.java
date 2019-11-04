b53e5d656b417ec2c08ab97fbbbefe8d064ac486
Robert Williams <robert.williams@yoti.com>
2019-11-04 10:26:41 +0000

    Add modification notice
    Needed for Apache compliance:
    You must cause any modified files to carry
    prominent notices stating that You changed the files

--- src/main/java/TestResult.java
+++ src/main/java/TestResult.java
@@ -1,4 +1,8 @@
 /*
+ * Modified for https://github.com/jamesknowsbest/Instrumentationpretty
+ * Original license follows
+ */
+/*
  * Copyright (C) 2010 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
